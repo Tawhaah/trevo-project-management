cd095f4b5f20e2244ec37e01b366dc2195f0ee15
Petri Tuononen <petri.tuononen@gmail.com>
2011-11-16 04:22:14 +0200

    Forgot to version DashboardModel

---
+++ PrjctMngmt/PrjctMngmt.WebUI/Models/DashboardModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PrjctMngmt.Models
+{
+    public class DashboardModel
+    {
+        //task
+        public int TotalTasks { get; set; }
+        public int FinishedTasks { get; set; }
+        public int UnfinishedTasks { get; set; }
+
+        //project
+        public int TotalProjects { get; set; }
+        public int FinishedProjects { get; set; }
+        public int UnfinishedProjects { get; set; }
+
+        //developer
+        public int TotalDevelopers { get; set; }
+    }
+}
