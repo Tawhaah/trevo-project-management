4695a34c6087807e799438c0efe05cc093a91388
Petri Tuononen <petri.tuononen@gmail.com>
2011-11-16 04:21:22 +0200

    Overview on dashboard.

--- PrjctMngmt/PrjctMngmt.WebUI/Controllers/DashboardController.cs
+++ PrjctMngmt/PrjctMngmt.WebUI/Controllers/DashboardController.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PrjctMngmt.Models;
 
 namespace PrjctMngmt.Controllers
 {
     public class DashboardController : Controller
     {
+        private EntityModelContainer db = new EntityModelContainer();
+        private DashboardModel model = new DashboardModel();
+
         //
         // GET: /Dashboard/
 
         public ActionResult Index()
         {
-            return View();
+            model.TotalTasks = GetAmountOfTotalTasks();
+            model.FinishedTasks = GetAmountOfFinishedTasks();
+            model.UnfinishedTasks = GetAmountOfUnfinishedTasks();
+
+            model.TotalProjects = GetAmountOfTotalProjects();
+            model.FinishedProjects = GetAmountOfFinishedProjects();
+            model.UnfinishedProjects = GetAmountOfUnfinishedProjects();
+
+            model.TotalDevelopers = GetAmountOfDevelopers();
+
+            return View(model);
         }
 
         //
@@ -100,6 +114,67 @@ namespace PrjctMngmt.Controllers
             {
                 return View();
             }
+        }
+
+        public int GetAmountOfUnfinishedProjects()
+        {
+            int amount = db.Projects.ToArray().Length -
+                db.Projects.Where(p => p.Status.Contains("Finished")).ToArray().Length;
+            return amount;
+        }
+
+        public int GetAmountOfFinishedProjects()
+        {
+            var projects = db.Projects.Where(t => t.Status.Contains("Finished"));
+            int amount = projects.ToArray().Length;
+            return amount;
+        }
+
+        public int GetAmountOfTotalProjects()
+        {
+            int amount = db.Projects.ToArray().Length;
+            return amount;
+        }
+
+        public int GetAmountOfFinishedTasks()
+        {
+            //byte values cannot be compared with LINQ ... therefore
+            List<Task> tasks = db.Tasks.ToList();
+            int amount = 0;
+            foreach(Task task in tasks) 
+            {
+                if (task.Finished == 1)
+                {
+                    amount++;
+                }
+            }
+            return amount;
+        }
+
+        public int GetAmountOfUnfinishedTasks()
+        {
+            //byte values cannot be compared with LINQ ... therefore
+            List<Task> tasks = db.Tasks.ToList();
+            int amount = 0;
+            foreach (Task task in tasks)
+            {
+                if (task.Finished == 0)
+                {
+                    amount++;
+                }
+            }
+            return amount;
+        }
+
+        public int GetAmountOfTotalTasks()
+        {
+            int amount = db.Tasks.ToArray().Length;
+            return amount;
+        }
+
+        public int GetAmountOfDevelopers()
+        {
+            return db.Developers.ToArray().Length;
         }
     }
 }
